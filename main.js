e24c7fec5f960f97f6b746fce4fdfe4c8cdef6e4
rogeriohenning <128552442+rogeriohenning@users.noreply.github.com>
2023-10-17 19:34:21 -0300

    .2

--- main.js
+++ main.js
@@ -2,4 +2,4 @@ function tocaSomVitoria(){
     document.querySelector('#som_tecla_vitoria').play();
 }
 const listaDeTeclas= document.querySelectorAll(".tecla");
-listaDeTeclas[0].onclick=tocaSomVitoria;
+listaDeTeclas[8].onclick=tocaSomVitoria;
