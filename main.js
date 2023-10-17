fd9ab1f45c5999ba51240386f9f5a1cc6002c931
rogeriohenning <128552442+rogeriohenning@users.noreply.github.com>
2023-10-17 19:47:20 -0300

    vaia

--- main.js
+++ main.js
@@ -4,7 +4,12 @@ function tocaSomVitoria(){
 const listaDeTeclas= document.querySelectorAll(".tecla");
 listaDeTeclas[8].onclick=tocaSomVitoria;
 
-function tocaSomAplauso(){
+function tocaSomAplausos(){
     document.querySelector('#som_tecla_aplausos').play();
 }
-listaDeTeclas[0].onclick=tocaSomAplauso;
+listaDeTeclas[0].onclick=tocaSomAplausos;
+
+function tocaSomVaia(){
+    document.querySelector('#som_tecla_vaia').play();
+}
+listaDeTeclas[1].onclick=tocaSomVaia;
