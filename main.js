41827146aa683aae77f7537725564a381470c3bc
rogeriohenning <128552442+rogeriohenning@users.noreply.github.com>
2023-10-17 19:43:26 -0300

    somaplauso

--- main.js
+++ main.js
@@ -3,3 +3,8 @@ function tocaSomVitoria(){
 }
 const listaDeTeclas= document.querySelectorAll(".tecla");
 listaDeTeclas[8].onclick=tocaSomVitoria;
+
+function tocaSomAplauso(){
+    document.querySelector('#som_tecla_aplauso').play();
+}
+listaDeTeclas[0].onclick=tocaSomAplauso;
