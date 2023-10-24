02788dd5ff8cc7524b5697d45fc6cd9fe6b295d5
rogeriohenning <128552442+rogeriohenning@users.noreply.github.com>
2023-10-24 18:38:25 -0300

    aula24/10.1

--- main.js
+++ main.js
@@ -1,15 +1,5 @@
-function tocaSomVitoria(){
-    document.querySelector('#som_tecla_vitoria').play();
-}
-const listaDeTeclas= document.querySelectorAll(".tecla");
-listaDeTeclas[8].onclick=tocaSomVitoria;
-
 function tocaSomAplausos(){
     document.querySelector('#som_tecla_aplausos').play();
 }
-listaDeTeclas[0].onclick=tocaSomAplausos;
-
-function tocaSomVaia(){
-    document.querySelector('#som_tecla_vaia').play();
-}
-listaDeTeclas[1].onclick=tocaSomVaia;
+const listaDeTeclas= document.querySelectorAll(".tecla");
+listaDeTeclas[8].onclick=tocaSomAplausos;
