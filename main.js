698faf2a3236432a636e51195b1b65b5cb5f0baf
rogeriohenning <128552442+rogeriohenning@users.noreply.github.com>
2023-10-24 18:47:01 -0300

    .2

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function tocaSomAplausos(){
 const listaDeTeclas= document.querySelectorAll(".tecla");
 
 let contador=0;
-while(contador < 8){
+while(contador < 9){
     listaDeTeclas[contador].onclick=tocaSomAplausos;
     contador=contador + 1;
     console.log(contador);
