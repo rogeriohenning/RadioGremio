a49d07b83066010bd1291e2ab901236943df95d8
rogeriohenning <128552442+rogeriohenning@users.noreply.github.com>
2023-10-24 19:07:20 -0300

    .7

--- main.js
+++ main.js
@@ -5,8 +5,10 @@ const listaDeTeclas= document.querySelectorAll(".tecla");
 
 let contador=0;
 while(contador < listaDeTeclas.length){
+    const efeito=listaDeTeclas[contador].classList[1];
+    const idAudio='#som_'+efeito;
     listaDeTeclas[contador].onclick=function(){
-        tocaSom("som_tecla_aplausos");
+        tocaSom(idAudio);
     }
     contador=contador + 1;
     console.log(contador);
