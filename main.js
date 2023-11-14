2498f4f256fe17b4d65b4b6fda58ee91291442cd
rogeriohenning <128552442+rogeriohenning@users.noreply.github.com>
2023-11-14 19:14:44 -0300

    .4

--- main.js
+++ main.js
@@ -11,7 +11,10 @@ for(let contador = 0; contador < listaDeTeclas.length; contador++){
         tocaSom(idAudio);
     }
     tecla.onkeydown=function(){
-        tecla.classList.add('ativa');
+        if (EventCounts.code !='Tab'){
+            tecla.classList.add('ativa');
+        }
+        
     }
     tecla.onkeyup=function(){
         tecla.classList.remove('ativa');
