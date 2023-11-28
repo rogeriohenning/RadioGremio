ba98031219582af494a488b9e4ad6dd3ca0eafc3
rogeriohenning <128552442+rogeriohenning@users.noreply.github.com>
2023-11-28 19:12:04 -0300

    .1

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 function tocaSom(idElementoAudio){
-    document.querySelector(idElementoAudio).play();
+    const elemento = document.querySelector(idElementoAudio);
+    if(elemento && elemento.localName==='audio'){
+        elemento.play();
+    }else{
+        console.log("Elemento não encontrado ou seletor inválido");
+    }
+    
 }
 const listaDeTeclas= document.querySelectorAll(".tecla");
 
